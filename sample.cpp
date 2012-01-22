b72bbec47e1bbd684ad258ad64c1b702f697f23a
Wiktor Wojtylak <wiktor.wojtylak@gmail.com>
2012-01-22 16:55:33 +0100

    sample.cpp formatting fix

--- sample.cpp
+++ sample.cpp
@@ -12,28 +12,28 @@ using namespace Eigen;
 int main(int argc, char* argv[])
 {
 
-	vector<vector<string> > inputData;
-	string line;
-	vector<string> lineSplit;
-	while(!getline(cin, line).eof()) {
-  		split(lineSplit, line, is_any_of(","));
-		inputData.push_back(lineSplit);
-	}
-	
+    vector<vector<string> > inputData;
+    string line;
+    vector<string> lineSplit;
+    while(!getline(cin, line).eof()) {
+        split(lineSplit, line, is_any_of(","));
+        inputData.push_back(lineSplit);
+    }
+    
     int n = inputData.size();
     int m = inputData.at(0).size();
     MatrixXf data(n, m);
-	
-	for (unsigned int i = 0; i < inputData.size(); ++i) {
-		lineSplit = inputData.at(i);
-		for (unsigned int j = 0; j < lineSplit.size(); ++j) {
-			data(i, j) = atof(lineSplit.at(j).c_str());
-		}
-	}
+    
+    for (unsigned int i = 0; i < inputData.size(); ++i) {
+        lineSplit = inputData.at(i);
+        for (unsigned int j = 0; j < lineSplit.size(); ++j) {
+            data(i, j) = atof(lineSplit.at(j).c_str());
+        }
+    }
     
     PcaSolver pcaSolver(true);
     MatrixXf reducedData = pcaSolver.performPcaOnData(data);
     cout << "reduced data: \n" << reducedData << endl;
-	
+    
     return 0;
 }
